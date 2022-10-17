39581a10c1cdf77c919590104219e917b66d6c05
20021386-NguyenDucHoangLong <pleasewaitlong@gmail.com>
2022-10-17 07:12:43 +0700

    update front-end
    - finish homepage
    - fix video error
    - add carditem.js
    - add cards.css
    - add cards.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,8 @@ import video from '../videos/video-1.mp4'
 function HeroSection() {
   return (
     <div class='hero-container'>
-        <video src={require('../videos/video-1.mp4').default} autoPlay loop muted/>
+        {/* <video src={require('../videos/video-1.mp4').default} autoPlay loop muted/> */}
+        <video src={video} autoPlay loop muted/>
         <h1>ADVENTURE AWAITS</h1>
         <p>What are you waiting for?</p>
         <div className='hero-btns'>
